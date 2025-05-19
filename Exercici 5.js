3587f52472af9b840095fcd808ced73f33f0de85
BauveR <rick.bauve@gmail.com>
2025-05-19 12:47:25 +0200

    Exercici 5 string to Uppercase

--- Exercici 5.js
+++ Exercici 5.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-    <script>
-
 function eachElement (minus, callbackFunction) {
     minus = minus.toUpperCase();
         callbackFunction (minus);
@@ -22,8 +11,3 @@ function processElement(minus) {
 
 eachElement(minus,processElement);
 
-
-
-    </script>
-</body>
-</html>
